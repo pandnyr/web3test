8532b5b85166cf41894448e4c2ba16f0675f7851
Emin Ibrahimoglu <pandnyr@hotmail.com>
2023-05-21 17:46:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Connect2Phantom from './components/Connect2Phantom';
+import { Wallet } from './components/Wallets';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Web3Test</h1>
+        <hr className="fullWidth" />
+        <Connect2Phantom></Connect2Phantom>
+
       </header>
     </div>
   );
