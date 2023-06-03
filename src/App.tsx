8abc11f7ed1fa43d3d6e4f8e24c762dfbdc507ec
Emin Ibrahimoglu <pandnyr@hotmail.com>
2023-06-03 17:38:26 +0300

    example list

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,44 @@ function App() {
         <a><Wallet></Wallet></a>
         </nav>
       </header>
+      <body>
+        <div className='tokenlist'>
+          <tr>
+          <td><div className='flexiblecontentstart'>#</div></td>
+          <td><div className='flexiblecontentop'>Ad</div></td>
+          <td><div className='flexiblecontentop'>Fiyat</div></td>
+          <td><div className='flexiblecontentop'>24H%</div></td>
+          <td><div className='flexiblecontentop'>7D%</div></td>
+          <td><div className='flexiblecontentop'>Piyasa Değeri</div></td>
+          <td><div className='flexiblecontentop'>Hacim</div></td>
+          <td><div className='flexiblecontentop'>Dolaşan Arz</div></td>
+          </tr>
+        </div>
+        <div className='tokenlist'>
+        <tr>
+          <td><div className='flexiblecontentstart'>1</div></td>
+          <td><div className='flexiblecontent'>BTC</div></td>
+          <td><div className='flexiblecontent'>69000</div></td>
+          <td><div className='flexiblecontent'>⬆️0.39%</div></td>
+          <td><div className='flexiblecontent'>⬆️1.39%</div></td>
+          <td><div className='flexiblecontent'>11,039,961,487,761 TL</div></td>
+          <td><div className='flexiblecontent'>238,616,342,029</div></td>
+          <td><div className='flexiblecontent'>19,391,706 BTC</div></td>
+          </tr>
+          </div>
+        <div className='tokenlist'>
+        <tr>
+          <td><div className='flexiblecontentstart'>2</div></td>
+          <td><div className='flexiblecontent'>ETH</div></td>
+          <td><div className='flexiblecontent'>25000</div></td>
+          <td><div className='flexiblecontent'>⬆️0.67%</div></td>
+          <td><div className='flexiblecontent'>⬆️1.24%</div></td>
+          <td><div className='flexiblecontent'>9,087,581,897,702 TL</div></td>
+          <td><div className='flexiblecontent'>132,315,244,567</div></td>
+          <td><div className='flexiblecontent'>21,994,566 ETH</div></td>
+          </tr>
+          </div>
+      </body>
     </div>
   );
 }
